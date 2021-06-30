b0c1cd744b558cd3eec1fc6a75cc7fe6cb60385d
youssef-elassri <youssef3.elassri@gmail.com>
2021-06-30 13:34:09 +0100

    fixed clients

--- models/Clients.py
+++ models/Clients.py
@@ -6,10 +6,6 @@ class Client(models.Model):
 
     name = fields.Char('nom')
     prenom = fields.Char('prenom')
-    date_naiss = fields.Date('Date de Naissance')
-    date_rej = fields.Date('Date')
-    salaire = fields.Float('salaire')
-    fonc = fields.Char('fonc')
 
     Orders = fields.One2many(
         'test_magasin.order', 'demandeur', string='Orders ')
